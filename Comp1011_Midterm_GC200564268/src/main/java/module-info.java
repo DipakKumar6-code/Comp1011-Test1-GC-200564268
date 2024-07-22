8f8d7fc125de84c78b01a1b73baeb9de59ec6bd1
DipakKumar6-code <126050058+DipakKumar6-code@users.noreply.github.com>
2024-07-21 22:29:39 -0400

    Delete Comp1011_Midterm_GC200564268 directory

--- Comp1011_Midterm_GC200564268/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.comp1011_midterm_gc200564268 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.comp1011_midterm_gc200564268 to javafx.fxml;
-    exports com.example.comp1011_midterm_gc200564268;
-}
